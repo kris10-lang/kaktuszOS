054aca0ff1f255908807c1e168dc3d1bc57e8ea3
kk <kristofkun10@gmail.com>
2025-12-02 18:53:36 +0100

    Delete sh directory

--- sh/compile.sh
+++
@@ -1,4 +0,0 @@
-nasm -f elf32 ./sources/kernel_entry.asm -o ./nomindbin/kernel_entry.o
-gcc -m32 -ffreestanding -c ./kernel/kernel.c -o ./nomindbin/kernel.o
-ld -m elf_i386 -T ./sources/linker.ld -o ./nomindbin/kernel.elf ./nomindbin/kernel_entry.o ./nomindbin/kernel.o
-objcopy -O binary ./nomindbin/kernel.elf kernel.bin
--- sh/install.sh
+++
@@ -1,8 +0,0 @@
-# Létrehozunk egy üres floppy image-t (pl. 1.44 MB = 2880 szektor * 512 bájt)
-dd if=/dev/zero of=./disk.img bs=512 count=2880
-
-# Beleírjuk az első szektorba a bootloadert
-dd if=./boot.bin of=./disk.img bs=512 count=1 conv=notrunc
-
-# Beleírjuk a kernel bináris fájlt a 2. szektortól kezdve
-dd if=./kernel.bin of=./disk.img bs=512 seek=1 conv=notrunc
